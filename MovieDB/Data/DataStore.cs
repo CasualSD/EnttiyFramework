d9fe324dcaacb9e47ce781c0916d0368b41bc4e7
CasualSD <93326155+CasualSD@users.noreply.github.com>
2022-02-01 14:56:33 +0100

    EFW

---
+++ MovieDB/Data/DataStore.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieDB.Data
+{
+    class ProjectData
+    {
+        public Models.User UserDetails { get; set; } = new Models.User();
+        public List<Models.Movie> Movies { get; set; } = new List<Models.Movie>();
+    }
+
+    class DataStore
+    {
+        ProjectData data;
+
+        public Models.User User => data.UserDetails;
+        public List<Models.Movie> Movies => data.Movies;
+
+        public void SetUser(Models.User user)
+        {
+            data.UserDetails = user;
+        }
+
+        public void AddMovie(Models.Movie movie)
+        {
+            data.Movies.Add(movie);
+        }
+
+        public DataStore()
+        {
+            if(System.IO.File.Exists("data.json"))
+            {
+                var input = System.IO.File.ReadAllText("data.json");
+                data = JsonConvert.DeserializeObject<ProjectData>(input);
+            } else
+            {
+                data = new ProjectData();
+            }
+        }
+
+        public void SaveData()
+        {
+            var content = JsonConvert.SerializeObject(data, Formatting.Indented);
+            System.IO.File.WriteAllText("data.json", content);
+        }
+    }
+}
---
+++ MovieDB/Program.cs
@@ -0,0 +1,35 @@
+﻿using MovieDB.Data;
+using MovieDB.Views;
+using System;
+
+namespace MovieDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a datastore
+            var store = new DataStore();
+
+            // create a menu
+            var menu = new SMUtils.Menu();
+
+            // call view to create user, and pass the new user to the store
+            menu.AddOption('1', "Set User Details", () => store.SetUser(UserView.CreateUser()));
+            // call view to show user, and pass the store user as an argument
+            menu.AddOption('2', "Show User Details", () => UserView.ShowUser(store.User));
+
+            // call view to create a movie, and use a function to add it to the store
+            menu.AddOption('3', "Add Movie", () => store.AddMovie(MovieView.CreateMovie()));
+
+            // call view to show movies and pass the list of movies from the store
+            menu.AddOption('4', "Show Movies", () => MovieView.ShowMovies(store.Movies));
+
+            // start running the menu
+            menu.Start();
+
+            // we get here after exiting the menu. Save the data to a json file
+            store.SaveData();
+        }
+    }
+}
